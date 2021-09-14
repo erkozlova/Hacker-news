ed93cd876e5cb62cce7fddaa5e9e2a6cb46f9c6f
erkozlova <eerrkozlova@gmail.com>
2021-09-14 15:53:15 +0300

    Added HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import "./index.css";
 import { App } from "./App";
@@ -10,13 +10,13 @@ import { theme } from "./theme/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Provider store={store}>
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
